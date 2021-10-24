a3e7c658ecde937d6c06be827f81043a4e58bda8
luiz-gustavo-ribeiro <email.gustavo.ribeiro@gmail.com>
2021-10-24 12:22:25 -0300

    finalizado, agora precisa melhorar visual

--- app.py
+++ app.py
@@ -39,16 +39,44 @@ def new():
 @app.route("/resp")
 def resp():
     equipamentos = Equipamento.query.all()
-    un = db.session.query(Equipamento.nome).filter(True == True).order_by(desc(Equipamento.id)).first()
-    uc = db.session.query(Equipamento.consumo).filter(True == True).order_by(desc(Equipamento.id)).first()
-    sumcotv = db.session.query(func.sum(Equipamento.consumo)).filter(Equipamento.nome=='Televisão').first()
-    sumtetv = db.session.query(func.sum(Equipamento.tempo)).filter(Equipamento.nome=='Televisão').first()
-    medge = db.session.query(func.avg(Equipamento.consumo)).filter(Equipamento.nome=='Geladeira').first()
-    medlr = db.session.query(func.avg(Equipamento.consumo)).filter(Equipamento.nome=='Lava Roupas').first()
-    medmo = db.session.query(func.avg(Equipamento.consumo)).filter(Equipamento.nome=='Microondas').first()
-    medac = db.session.query(func.avg(Equipamento.consumo)).filter(Equipamento.nome=='Ar Condicionado').first()
-    return render_template('resp.html', un=un, uc=uc, sumcotv=sumcotv, sumtetv=sumtetv, equipamentos=equipamentos, medge=medge, medlr=medlr, medmo=medmo, medac=medac)
-    
+    un = db.session.query(Equipamento.nome).filter(Equipamento.id == db.session.query(func.max(Equipamento.id))).scalar()
+    uc = db.session.query(Equipamento.consumo).filter(Equipamento.id == db.session.query(func.max(Equipamento.id))).scalar()
+    ut = db.session.query(Equipamento.tempo).filter(Equipamento.id == db.session.query(func.max(Equipamento.id))).scalar()
+    meduc = uc / ut
+    sumcotv = db.session.query(func.sum(Equipamento.consumo)).filter(Equipamento.nome=='Televisão').scalar()
+    sumtetv = db.session.query(func.sum(Equipamento.tempo)).filter(Equipamento.nome=='Televisão').scalar()
+    medtv = sumcotv / sumtetv
+    sumcoge = db.session.query(func.sum(Equipamento.consumo)).filter(Equipamento.nome=='Geladeira').scalar()
+    sumtege = db.session.query(func.sum(Equipamento.tempo)).filter(Equipamento.nome=='Geladeira').scalar()
+    medge = sumcoge / sumtege
+    sumcolr = db.session.query(func.sum(Equipamento.consumo)).filter(Equipamento.nome=='Lava Roupas').scalar()
+    sumtelr = db.session.query(func.sum(Equipamento.tempo)).filter(Equipamento.nome=='Lava Roupas').scalar()
+    medlr = sumcolr / sumtelr
+    sumcomo = db.session.query(func.sum(Equipamento.consumo)).filter(Equipamento.nome=='Microondas').scalar()
+    sumtemo = db.session.query(func.sum(Equipamento.tempo)).filter(Equipamento.nome=='Microondas').scalar()
+    medmo = sumcomo / sumtemo
+    sumcoac = db.session.query(func.sum(Equipamento.consumo)).filter(Equipamento.nome=='Ar Condicionado').scalar()
+    sumteac = db.session.query(func.sum(Equipamento.tempo)).filter(Equipamento.nome=='Ar Condicionado').scalar()
+    medac = sumcoac / sumteac
+    if (un == 'Televisão') and (meduc > medtv):
+        conc = "O consumo de sua televisão está acima da média"
+    if (un == 'Televisão') and (meduc <= medtv):
+        conc = "O consumo de sua televisão está dentro do esperado"
+    if (un == 'Geladeira') and (meduc > medge):
+        conc = "O consumo de sua geladeira está acima da média"
+    if (un == 'Geladeira') and (meduc <= medge):
+        conc = "O consumo de sua geladeira está dentro do esperado"
+    if (un == 'Lava Roupas') and (meduc > medlr):
+        conc = "O consumo de sua Lava Roupas está acima da média"
+    if (un == 'Lava Roupas') and (meduc <= medlr):
+        conc = "O consumo de sua Lava Roupas está dentro do esperado"
+    if (un == 'Microondas') and (meduc > medmo):
+        conc = "O consumo de seu Micro-ondas está acima da média"
+    if (un == 'Microondas') and (meduc <= medmo):
+        conc = "O consumo de seu Micro-ondas está dentro do esperado"
+
+    return render_template('resp.html', conc=conc, un=un, meduc=meduc, medtv=medtv, equipamentos=equipamentos, medge=medge, medlr=medlr, medmo=medmo, medac=medac)
+  
 
 if __name__ =='__main__':
     db.create_all()
